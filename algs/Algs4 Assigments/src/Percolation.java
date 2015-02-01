38bc0ada2b6e5fea7285ce45b32430793dbb6ee2
Lucas de Castro Oliveira <lcastro.oliveira@acm.org>
2015-02-02 12:01:02 +1300

    Assignment done, test cases and visualizer added
    Score: 84
    Improvements to be done:
    Backwash bug
    Corner Case
    Fix intervals on Stats

--- algs/Algs4 Assigments/src/Percolation.java
+++ algs/Algs4 Assigments/src/Percolation.java
@@ -10,19 +10,14 @@ public class Percolation {
 	public Percolation(int N) throws IllegalArgumentException {
 		if(N <= 0) throw new IllegalArgumentException();
 		this.N = N;
-		
 		uf = new WeightedQuickUnionUF((N*N)+2);
 		grid = new int[N*N+2];
 		
-		grid[0] = 0;
-		grid[N*N-1] = 0;
-		for(int i = 1; i < grid.length; i++) {
-			grid[i] = -1;
-		}
+		grid[0] = 1;
+		grid[N*N+1] = 1;
 		
 		for(int i = 1; i <= N; i++) {
 			uf.union(0, i);
-			
 		}
 		
 		for(int i = (N-1)*N+1; i <= (N-1)*N+N; i++) {
@@ -32,43 +27,67 @@ public class Percolation {
 	}
 	
 	// open site (row i, column j) if it is not open already
-	public void open(int i, int j) {
-		validateIndex(i);
-		validateIndex(j);
-		grid[xyTo1D(i, j)] = 0;
+	public void open(int i, int j) throws IndexOutOfBoundsException  {
+		if(!validateIndex(i)) throw new IndexOutOfBoundsException("row index i out of bounds");
+		if(!validateIndex(j)) throw new IndexOutOfBoundsException("row index j out of bounds");
+		if(N > 1) {
+			grid[xyTo1D(i, j)] = 1;
+			
+			//Up
+			if(i - 1 >= 1 && isOpen(i-1, j)) {
+				uf.union(xyTo1D(i, j), xyTo1D(i-1, j));
+			}
+			
+			//Left
+			if(j - 1 >= 1 && isOpen(i, j-1)) {
+				uf.union(xyTo1D(i, j), xyTo1D(i, j-1));
+			}
+			
+			//Right
+			if(j + 1 <= N && isOpen(i, j+1)) {
+				uf.union(xyTo1D(i, j), xyTo1D(i, j+1));
+			}
+			
+			//Down
+			if(i + 1 <= N && isOpen(i+1, j)) {
+				uf.union(xyTo1D(i, j), xyTo1D(i+1, j));
+			}
+		} else {
+			uf.union(0, 1);
+			uf.union(2, 1);
+		}
 	}
 	
 	// is site (row i, column j) open?
 	public boolean isOpen(int i, int j) {
-		validateIndex(i);
-		validateIndex(j);
-		if(grid[xyTo1D(i, j)] == 0) return true;
+		if(!validateIndex(i)) throw new IndexOutOfBoundsException("row index i out of bounds");
+		if(!validateIndex(j)) throw new IndexOutOfBoundsException("row index j out of bounds");
+		if(grid[xyTo1D(i, j)] == 1) return true;
 		else return false;
 	}
 	
 	// is site (row i, column j) full?
-	public boolean isFull(int i, int j) {
-		validateIndex(i);
-		validateIndex(j);
-		return uf.connected(0, xyTo1D(i, j));
+	public boolean isFull(int i, int j) throws IndexOutOfBoundsException {
+		if(!validateIndex(i)) throw new IndexOutOfBoundsException("row index i out of bounds");
+		if(!validateIndex(j)) throw new IndexOutOfBoundsException("row index j out of bounds");
+		if(isOpen(i, j)) return uf.connected(0, xyTo1D(i, j));
+		else return false;
 	}
 	
 	// does the system percolate?
 	public boolean percolates() {
-		return false;
+		return uf.connected(0, N*N+1);
 	}
 	
-	private boolean validateIndex(int index) throws IndexOutOfBoundsException {
-		if (index <= 0 || index > N*N) throw new IndexOutOfBoundsException("row index i out of bounds");
+	private boolean validateIndex(int index) {
+		if (index <= 0 || index > N) return false; 
 		else return true;
 	}
 	
 	private int xyTo1D(int x, int y) {
 		return (x-1)*N+y;
 	}
-
+	
 	// test client (optional)
-	public static void main(String[] args) {
-		
-	}
+	public static void main(String[] args) {}
 }
--- algs/Algs4 Assigments/src/PercolationStats.java
+++ algs/Algs4 Assigments/src/PercolationStats.java
@@ -1,33 +1,55 @@
 
 public class PercolationStats {
-	
-   // perform T independent experiments on an N-by-N grid
-   public PercolationStats(int N, int T) throws IllegalArgumentException {
-	   if(N <= 0 || T <= 0) throw new IllegalArgumentException();
-   }
-   
-   // sample mean of percolation threshold
-   public double mean() {
-	   return 0.0;
-   }
-   
-   // sample standard deviation of percolation threshold
-   public double stddev() {
-	   return 0.0;
-   }
-   
-   // low  endpoint of 95% confidence interval
-   public double confidenceLo() {
-	   return 0.0;
-   }
-   
-   // high endpoint of 95% confidence interval
-   public double confidenceHi() {
-	   return 0.0;
-   }
 
-   // test client
-   public static void main(String[] args) {
-	   
-   }
+	private double results[];
+	private double mean;
+	private double std;
+	private int T;
+	private int openSites = 0;
+
+	// perform T independent experiments on an N-by-N grid
+	public PercolationStats(int N, int T) throws IllegalArgumentException {
+		if(N <= 0 || T <= 0) throw new IllegalArgumentException();
+		this.T = T;
+		results = new double[T];
+		for(int i = 0; i < T; i++) {
+			Percolation p = new Percolation(N);
+			openSites = 0;
+			while(!p.percolates()) {
+				int x = StdRandom.uniform(1, N+1);
+				int y = StdRandom.uniform(1, N+1);
+				if(!p.isOpen(x, y)) {
+					p.open(x,y);
+					openSites++;
+				}
+			}
+			results[i] = ((double)openSites/(N*N));
+		}
+	}
+
+	// sample mean of percolation threshold
+	public double mean() {
+		mean = StdStats.mean(results);
+		return mean;
+	}
+
+	// sample standard deviation of percolation threshold
+	public double stddev() {
+		std = StdStats.stddev(results);
+		return std;
+	}
+
+	// low  endpoint of 95% confidence interval
+	public double confidenceLo() {
+		return (mean - ((1.96*Math.sqrt(std))/Math.sqrt(T)));
+	}
+
+	// high endpoint of 95% confidence interval
+	public double confidenceHi() {
+		return (mean + ((1.96*Math.sqrt(std))/Math.sqrt(T)));
+	}
+
+	// test client
+	public static void main(String[] args) {
+	}
 }
